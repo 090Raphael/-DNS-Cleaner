9e8ce378bab3d00fd4bb970189ba1c8b12f0a594
Raphael C <159970639+RC024@users.noreply.github.com>
2024-04-07 13:15:45 -0300

    Update FLUSH-DNS.bat

--- FLUSH-DNS.bat
+++ FLUSH-DNS.bat
@@ -1,23 +1,38 @@
-@ECHO
-IPCONFIG /all
-@ECHO 
-ipconfig /release 
-pause
-ipconfig /renew 
-pause
-ipconfig /flushdns 
-pause
-ipconfig /registerdns 
-pause
-netsh dump 
-pause
-nbtstat -R 
-pause
-netsh int ip reset reset.log 
-pause
-netsh winsock reset 
-pause
- to join this conver
-
-PAUSE
+chcp 65001
+@echo off
+echo.
+@color 1a
+@title FLUSHDNS
+cls
+@title Limpar seu cache DNS / Erros de navegação
+cls
+echo ==================================
+echo    Siga as instruções a seguir
+echo ==================================
+echo.
+echo.
+echo Agora vamos começar o processo para alterar seu endereço IP na rede ?
+pause>nul
+cls
+echo Deseja alterar o IP do seu equipamento?
+echo (1) Sim
+echo (2) Não
+set /p resp=
+if "%resp%" == "1" (goto process) else (goto end)
+:process
+ipconfig /flushdns
+ipconfig /registerdns
+ipconfig /setclassid
+ipconfig /release
+ipconfig /renew
+netsh Winsock Reset
+netsh int ip Reset
+nbtstat -rr
+netsh int ip reset all
+echo.
+echo (Seu IP foi renovado) pressione qualquer tecla para sair.
+echo.
+pause >nul
+exit
+:end
 
